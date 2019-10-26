483fb2485658eceffa54f0e25f6b8973f4d3ad7a
dshaw001 <57046525+dshaw001@users.noreply.github.com>
2019-10-27 00:44:15 +0530

    areaofhexagon.cpp

---
+++ areaofhexagon.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+#include <math.h> 
+using namespace std; 
+int main() 
+{  
+    double s,t; 
+    cin>>s;
+    t=(3 * sqrt(3)*(s * s)) / 2;
+    cout << "Area : "
+         << t; 
+    return 0; 
+} 
